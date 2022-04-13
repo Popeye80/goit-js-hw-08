375930e3954bb92f3df43fa1ea74c479ea7f0b78
Vladimir Pavlov <91782115+Popeye80@users.noreply.github.com>
2022-04-13 15:28:41 +0300

    11

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview: previewImg, original: originalImg, description: alt }) =>
+      `
+                <a class="gallery__item" href="${originalImg}">
+                    <img class="gallery__image" src="${previewImg}" alt="${alt}"/>
+                </a>
+            `
+  )
+  .join("");
+
+galleryContainer.innerHTML = galleryMarkup;
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionType: "attr",
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox");
